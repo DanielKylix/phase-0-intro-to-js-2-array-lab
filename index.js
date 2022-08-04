0af8a0be5bc75baf79f8f9f2af60fc545c9ec048
Daniel <daniel@kylix.online>
2022-08-04 11:44:30 +0300

    new changes

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"]
+
+const destructivelyAppendCat = (name) => {
+    cats.push(name)
+    console.log(cats)
+
+}
+destructivelyAppendCat('Ralph')
+cats = ["Milo", "Otis", "Garfield"]
+
+const destructivelyPrependCat = (name) => {
+    cats.unshift(name)
+    console.log(cats)
+}
+destructivelyPrependCat ('Bob')
+cats = ["Milo", "Otis", "Garfield"]
+
+const destructivelyRemoveLastCat = () => {
+cats.pop()
+console.log(cats)
+
+}
+destructivelyRemoveLastCat ()
+cats = ["Milo", "Otis", "Garfield"]
+
+const destructivelyRemoveFirstCat = () => {
+cats.shift()
+console.log(cats)
+
+}
+destructivelyRemoveFirstCat ()
+cats = ["Milo", "Otis", "Garfield"]
+
+const appendCat = (name) => {
+   return [...cats,name]
+
+}
+appendCat('Broom')
+cats = ["Milo", "Otis", "Garfield"]
+
+const prependCat = (name) => {
+   return [name,...cats]
+
+}
+prependCat('Arnold')
+cats = ["Milo", "Otis", "Garfield"]
+
+const removeLastCat = () => {
+return cats.slice(0,2)
+}
+removeLastCat ()
+const removeFirstCat = () => {
+    return cats.slice(1)
+    }
+removeFirstCat ()
